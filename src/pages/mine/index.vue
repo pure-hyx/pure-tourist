97053c96decbf76caeddd734c762e41d139c9c6d
pure-hyx <1046562086@qq.com>
2020-01-03 16:18:12 +0800

    2020.1.3

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -6,18 +6,65 @@
     </view>
     <open-data type="userNickName"></open-data>
     </view>
+    <i-panel title="我要推荐">
+    <i-input :value="name" @change="changeName($event)" title="景点名称" autofocus placeholder="名称" maxlength="20"/>
+    <i-input :value="address" @change="changeAddress($event)" title="景点地址" placeholder="地址" maxlength="20" />
+    <i-input :value="remark" @change="changeRemark($event)" title="景点介绍" placeholder="介绍" maxlength="50" />
+    <i-button @click="handleClick()">我要推荐</i-button>
+    <i-toast id="toast" />
+    </i-panel>
   </div>
 </template>
 
 <script>
+const { $Toast } = require('../../../static/dist/base/index');
 
 export default {
   data () {
     return {
+      name: "",
+      address: "",
+      remark: ""
     }
   },
 
   methods: {
+    changeName(event) {
+      console.log(event)
+      this.name = event.mp.detail.detail.value
+    },
+    changeAddress(event) {
+      console.log(event)
+      this.address = event.mp.detail.detail.value
+    },
+    changeRemark(event) {
+      console.log(event)
+      this.remark = event.mp.detail.detail.value
+    },
+    handleClick() {
+      if (this.name && this.address && this.remark) {
+        let event = {
+          name: this.name,
+          address: this.address,
+          remark: this.remark,
+          image: 'cloud://edu-868a10.6564-edu-868a10/food/4.png'
+        }
+        wx.cloud.callFunction({ name: 'new_shop', data: event }).then(
+              res => {
+                console.log(res)
+                }
+        )
+        $Toast({
+            content: '数据已经提交',
+            type: 'success'
+        });
+      } else {
+        $Toast({
+            content: '请填写完整信息',
+            type: 'warning'
+        });
+      }
+    }
   },
 
   created () {
